0284f0d9bce266a1700a0797bcdeef2921af6f61
Nguyen Duc <dunguyen07022004@gmail.com>
2025-06-30 11:17:20 +0700

    fix hoa don detail

--- src/main/resources/templates/fashionshop-ui/src/App.jsx
+++ src/main/resources/templates/fashionshop-ui/src/App.jsx
@@ -12,7 +12,7 @@ import createCache from "@emotion/cache";
 import routes from "routes";
 import { useSoftUIController, setMiniSidenav} from "context";
 import brand from "assets/images/logo4.png";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function App() {
   const [controller, dispatch] = useSoftUIController();
   const { miniSidenav, direction, layout, sidenavColor } = controller;
