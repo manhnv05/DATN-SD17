b3ec30ca2572e09e9fc955aaf2b6fa8fda3ab422
Rimuru <181172366+manhnv05@users.noreply.github.com>
2025-08-08 04:34:04 +0700

    fix luông phân quyền

--- src/main/resources/templates/fashionshop-ui/src/layouts/routes/routes-client.jsx
+++ src/main/resources/templates/fashionshop-ui/src/layouts/routes/routes-client.jsx
@@ -1,5 +1,6 @@
 import Home from "layouts/client/home";
 import SignIn from "layouts/admin/authentication/sign-in"; // chỉnh lại path cho đúng nếu cần
+import SignUp from "layouts/admin/authentication/sign-up"; // chỉnh lại path cho đúng nếu cần
 
 const routesClient = [
     {
@@ -16,7 +17,16 @@ const routesClient = [
         route: "/authentication/sign-in",
         component: <SignIn />,
         noCollapse: true,
-        hidden: true, // Bạn có thể ẩn menu này nếu không muốn lộ ra ở client menu
+        hidden: true,
+    },
+    {
+      type: "collapse",
+      name: "Sign Up",
+      key: "sign-up",
+      route: "/authentication/sign-up",
+      component: <SignUp />,
+      noCollapse: true,
+        hidden: true,
     },
 ];
 
