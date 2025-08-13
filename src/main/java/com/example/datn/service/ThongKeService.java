e94dc623fa26d875e75d3a507a6732969a742342
trantan123 <tanthph43325@fpt.edu.vn>
2025-08-13 18:25:54 +0700

    fix thong ke

--- src/main/java/com/example/datn/service/ThongKeService.java
+++ src/main/java/com/example/datn/service/ThongKeService.java
@@ -4,16 +4,17 @@ import com.example.datn.dto.*;
 import com.example.datn.vo.thongKeVO.ThongKeVoSearch;
 import org.springframework.data.domain.Page;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 public interface ThongKeService {
-    Map<String, ThongKeDTO> getThongKe();
+    Map<String, ThongKeDTO> getThongKe(LocalDateTime bd, LocalDateTime kt);
 
     Page<ThongKeSPBanChayDTO> getThongKeSpBanChayByQuery(ThongKeVoSearch thongKeVoSearch, int page, int size);
 
-    Page<ChiTietSanPhamSapHetDTO> getAllChiTietSanPhamSapHetHan(int page, int size);
+    Page<ChiTietSanPhamSapHetDTO> getAllChiTietSanPhamSapHetHan(int page, int size, Integer slQuery);
 
-    ThongKeBieuDoDTO getBieuDo(int check);
+    ThongKeBieuDoDTO getBieuDo(int check,LocalDateTime bd, LocalDateTime kt);
 
     ThongKeBieuDoDTO getBieuDoByQuery(ThongKeVoSearch thongKeVoSearch);
 
