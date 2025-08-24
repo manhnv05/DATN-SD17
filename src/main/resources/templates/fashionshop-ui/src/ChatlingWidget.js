3275db3553944b1b51beb647fe126edda152cf93
trantan123 <tanthph43325@fpt.edu.vn>
2025-08-24 08:38:58 +0700

    chat

--- src/main/resources/templates/fashionshop-ui/src/ChatlingWidget.js
+++ src/main/resources/templates/fashionshop-ui/src/ChatlingWidget.js
@@ -2,18 +2,21 @@ import { useEffect } from "react";
 
 export default function ChatlingWidget() {
   useEffect(() => {
-    window.chtlConfig = {
-      chatbotId: "2796661159",
-    };
-
     const script = document.createElement("script");
-    script.src = "https://chatling.ai/js/embed.js";
-    script.async = true;
-    script.id = "chtl-script";
+    script.id = "messenger-widget-b";
+    script.src = "https://cdn.botpenguin.com/website-bot.js";
+    script.defer = true;
+
+    // Đây là phần nội dung trong <script>...</script>
+    const botContent = document.createTextNode(
+      "68a6db512d211603a972a75c,68a58dbff4310e67c802bdfa"
+    );
+
+    script.appendChild(botContent);
     document.body.appendChild(script);
 
     return () => {
-      const oldScript = document.getElementById("chtl-script");
+      const oldScript = document.getElementById("messenger-widget-b");
       if (oldScript) document.body.removeChild(oldScript);
     };
   }, []);
